ef90e71b77e79de77ed955b1f59e55deda0f9700
Shihyun Han <devowl99@gmail.com>
2024-11-23 02:12:25 +0900

    Initial commit

---
+++ src/main/java/danpoong/danpoong/DanpoongApplication.java
@@ -0,0 +1,13 @@
+package danpoong.danpoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanpoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanpoongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/danpoong/danpoong/DanpoongApplicationTests.java
@@ -0,0 +1,13 @@
+package danpoong.danpoong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanpoongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
